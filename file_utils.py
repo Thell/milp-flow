393f9bde58c81705cd29b67913f01f305679e5c9
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-07-09 12:42:53 -0500

    file_utils: add write helpers

--- file_utils.py
+++ file_utils.py
@@ -2,18 +2,16 @@ import json
 import os
 
 
-def read_data_json(data_path, filename):
-    filepath = os.path.join(os.path.dirname(__file__), "data", data_path, filename)
+def read_data_json(data_sub_path, filename):
+    filepath = os.path.join(os.path.dirname(__file__), "data", data_sub_path, filename)
     with open(filepath, "r") as file:
         return json.load(file)
 
 
-def read_user_json(filename):
-    return read_data_json("user", filename)
-
-
-def read_workerman_json(filename):
-    return read_data_json("workerman", filename)
+def write_data_json(data_sub_path, filename, data):
+    filepath = os.path.join(os.path.dirname(__file__), "data", data_sub_path, filename)
+    with open(filepath, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 def read_sample_json(filename):
@@ -21,6 +19,28 @@ def read_sample_json(filename):
 
 
 def write_sample_json(filename, data):
-    filepath = os.path.join(os.path.dirname(__file__), "data", "sample", filename)
-    with open(filepath, "w") as file:
-        json.dump(data, file, indent=4)
+    return write_data_json("sample", filename, data)
+
+
+def read_user_json(filename):
+    return read_data_json("user", filename)
+
+
+def write_user_json(filename, data):
+    return write_data_json("user", filename, data)
+
+
+def read_workerman_json(filename):
+    return read_data_json("workerman", filename)
+
+
+def read_empire_json(filename):
+    if filename.startswith("sample_"):
+        return read_sample_json(filename)
+    return read_user_json(filename)
+
+
+def write_empire_json(filename, data):
+    if filename.startswith("sample_"):
+        return write_sample_json(filename, data)
+    return write_user_json(filename, data)
