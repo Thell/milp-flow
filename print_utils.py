a8cb7bcd862b92886db8e806372a817ea29ecada
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-07-08 22:51:03 -0500

    print_utils: add flag for full output.

--- print_utils.py
+++ print_utils.py
@@ -1,44 +1,45 @@
 import pulp
 
 
-def print_solver_output(prob, data, edge_vars):
-    # Write model for debugging
-    prob.writeLP("sample_LP.lp")
+def print_solver_output(prob, data, edge_vars, doFull=False):
+    if doFull:
+        # Write model for debugging
+        prob.writeLP("sample_LP.lp")
 
-    # Print all decision variables with non-zero values
-    print("Decision Variables with values:")
-    for group in [
-        "flow_source",
-        "flow_prod",
-        "flow_city_",
-        "flow_active_prod",
-        "flow_transit",
-        "flow_warehouse",
-        "load_transit",
-        "has_load",
-    ]:
-        for v in prob.variables():
-            if v.name.startswith(group):
-                if v.varValue is not None and v.varValue > 0:
-                    print(v.name, "=", v.varValue)
+        # Print all decision variables with non-zero values
+        print("Decision Variables with values:")
+        for group in [
+            "flow_source",
+            "flow_prod",
+            "flow_city_",
+            "flow_active_prod",
+            "flow_transit",
+            "flow_warehouse",
+            "load_transit",
+            "has_load",
+        ]:
+            for v in prob.variables():
+                if v.name.startswith(group):
+                    if v.varValue is not None and v.varValue > 0:
+                        print(v.name, "=", v.varValue)
 
-    # Print the activated edges with their details
-    print("\nActivated Edges:")
-    for group in [
-        "source",
-        "prod",
-        "city_",
-        "active_prod",
-        "transit",
-        "warehouse",
-    ]:
-        for edge in data["edges"]:
-            if edge["source"].startswith(group):
-                var = edge_vars[(edge["source"], edge["destination"])]
-                if var.varValue is not None and var.varValue > 0:
-                    print(
-                        f"Edge from {edge['source']} to {edge['destination']} with flow {var.varValue} has value {edge['value']} and cost {edge['cost']}"
-                    )
+        # Print the activated edges with their details
+        print("\nActivated Edges:")
+        for group in [
+            "source",
+            "prod",
+            "city_",
+            "active_prod",
+            "transit",
+            "warehouse",
+        ]:
+            for edge in data["edges"]:
+                if edge["source"].startswith(group):
+                    var = edge_vars[(edge["source"], edge["destination"])]
+                    if var.varValue is not None and var.varValue > 0:
+                        print(
+                            f"Edge from {edge['source']} to {edge['destination']} with flow {var.varValue} has value {edge['value']} and cost {edge['cost']}"
+                        )
 
     # Output results
     total_value = 0
@@ -50,7 +51,7 @@ def print_solver_output(prob, data, edge_vars):
     for edge in data["edges"]:
         edge_var = edge_vars[(edge["source"], edge["destination"])]
         flow_value = pulp.value(edge_var)
-        if flow_value is None:
+        if flow_value is None and doFull:
             print(f"edge_var: {edge_var} is NONE")
             continue
         elif flow_value > 0:
@@ -66,9 +67,10 @@ def print_solver_output(prob, data, edge_vars):
             ):
                 city_id = int(edge["source"].split("_")[1])
                 city_active_prods[city_id].append(edge["destination"])
-            print(
-                f"Edge from {edge['source']} to {edge['destination']} with flow {flow_value} has value {edge['value']} and cost {edge['cost']}"
-            )
+            if doFull:
+                print(
+                    f"Edge from {edge['source']} to {edge['destination']} with flow {flow_value} has value {edge['value']} and cost {edge['cost']}"
+                )
 
     # Summary output
     print("\nSummary:")
