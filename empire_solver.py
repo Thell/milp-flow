41a66c84c6006af6fe28014168568305a1c2966e
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-09-15 22:43:56 -0500

    checkpoint: new best

--- empire_solver.py
+++ empire_solver.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 from pathlib import Path
+import random
 from typing import List
 
 from pulp import LpVariable, lpSum, LpProblem, LpMaximize, HiGHS_CMD
@@ -91,10 +92,9 @@ def create_problem(config, G):
 
     # Group specific f⁻𝓣 == f⁺𝓢
     link_in_out_by_group(prob, G["V"]["𝓣"], G["V"]["𝓣"].inbound_arcs, G["V"]["𝓢"].outbound_arcs)
-
     prob += G["V"]["𝓢"].vars["x"] == 1, "x_source"
 
-    # Enforce that all nodes maintain a minimum degree of 2.
+    # Active node neighborhood constraints.
     for node in G["V"].values():
         if node.type in [NT.S, NT.T]:
             continue
@@ -102,19 +102,12 @@ def create_problem(config, G):
         in_neighbors = [arc.source.vars["x"] for arc in node.inbound_arcs]
         out_neighbors = [arc.destination.vars["x"] for arc in node.outbound_arcs]
 
-        # Waypoint and plants always have 1 or more inbound neighbors than outbound neighbors
-        # which is sortof a misnomer since all waypoints have anti-parallel edges but for some
-        # reason this constraint improves performance. I think it is because of the zone edges.
-        if node.isWaypoint or node.isPlant:
+        if node.type in [NT.plant, NT.waypoint, NT.group]:
             prob += lpSum(in_neighbors) >= lpSum(out_neighbors)
-
-        # All nodes should be 2-degree.
         if node.isWaypoint:
             prob += lpSum(in_neighbors) - 2 * node.vars["x"] >= 0
         else:
             prob += lpSum(in_neighbors) + lpSum(out_neighbors) - 2 * node.vars["x"] >= 0
-
-        # Every active node must have at least one active outbound neighbor
         prob += lpSum(out_neighbors) >= node.vars["x"]
 
     return prob
@@ -148,14 +141,11 @@ def config_solver(config):
         "parallel=on",
         "threads=16",
         f"time_limit={time_limit}",
+        f"random_seed={config["solver"]["random_seed"]}",
         "mip_feasibility_tolerance=1e-4",  # 1e-6 from config file
         "primal_feasibility_tolerance=1e-4",  # 1e-6 from config file
         # f"mip_rel_gap={mips_gap}",  # 1e-5 from this main()'s config
-        "mip_pool_soft_limit=5000",  # Leaving near default seems best. 892.66 on 501 w/7500 904 w/5000 Both w/1e-6 feas and default feas
-        # "mip_improving_solution_save=on",
-        # f"mip_improving_solution_file={filepath.joinpath("improvements", f'{filename}.sol')}",
-        # f"write_model_file={filepath.joinpath("models", f"{filename}.lp")}",
-        # f"write_presolved_model_to_file=True",
+        "mip_pool_soft_limit=5000",
     ]
     options = [o for o in options if "default" not in o]
 
@@ -216,22 +206,22 @@ def print_solving_info_trailer(config, prob, G):
 def save_reproduction_data(config, prob, G):
     obj_value = round(prob.objective.value())
     filename = f"{config_filename(config)}_{obj_value}"
-    filepath = Path(os.path.dirname(__file__), "highs_output", "solutions")
+    filepath = Path(os.path.dirname(__file__), "highs_output")
 
-    new_path = filepath.joinpath(f"{filename}.lp")
+    new_path = filepath.joinpath("models", f"{filename}.lp")
     prob.writeLP(new_path)
     print("         LP model saved:", new_path)
 
-    new_path = filepath.joinpath(f"{filename}_pulp.json")
+    new_path = filepath.joinpath("solutions", f"{filename}_pulp.json")
     prob.to_json(new_path)
     print("Solved pulp model saved:", new_path)
 
     old_path = filepath.parent.parent.joinpath(f"{config["name"]}-pulp.sol")
-    new_path = filepath.joinpath(f"{filename}_highs.sol")
+    new_path = filepath.joinpath("solutions", f"{filename}_highs.sol")
     Path(old_path).rename(new_path)
     print("   HiGHS solution saved:", new_path)
 
-    new_path = filepath.joinpath(f"{filename}_cfg.json")
+    new_path = filepath.joinpath("solutions", f"{filename}_cfg.json")
     with open(new_path, "w") as file:
         json.dump(config, file, indent=4)
     print("   Problem config saved:", new_path)
@@ -260,33 +250,35 @@ def main(config):
     waypoint_capacity: max loads on a waypoint
     """
 
-    # for budget in [10, 30, 50, 100, 150, 200, 250, 300, 350, 400, 450, 501]:
-    #   5 =>         1       0         1 100.00%   13900824.11     13900824.11        0.00%       42      4     15       539     1.3s
-    #  10 =>         1       0         1 100.00%   24954483.03     24954483.03        0.00%      293     18     16      1136     0.7s
-    #  20 =>         1       0         1 100.00%   44334018.51     44334018.51        0.00%       48      5     18      2665     1.1s
-    #  30 =>         1       0         1 100.00%   58540725        58540725           0.00%       71     48    255     21461     8.7s
-    #  50 =>         7       0         4 100.00%   84340234.96     84340234.96        0.00%      175     87    730     44348    16.6s
-    # 100 =>      1865       0       814 100.00%   137568512.6988  137562435.03       0.00%     1311    222   4884    401490   134.5s
-    # 150 =>     12797       0      2652 100.00%   183029741.7658  183012954.78       0.01%     1540    335   4880     1971k   674.9s
-    # 200 =>     39435       0     18036 100.00%   222778830.6818  222756620.54       0.01%     2411    433   4846     9074k  2782.7s
-    # 250 =>     38701       0     15709 100.00%   260995924.6606  260969949.12       0.01%     2886    486   4868     7755k  2635.1s
-    # 300 =>     31674       0     11645 100.00%   297920432.924   297890668.77       0.01%     2253    463   4902     5684k  1804.5s
-    # 350 =>     25784       0     10578 100.00%   332070168.0292  332036997.45       0.01%     2154    338   5020     4910k  1630.9s
-    # 400 =>     19413       0      8167 100.00%   361375079.5188  361339029          0.01%     2528    501   4957     3624k  1233.6s
-    # 450 =>     37922       0     14616 100.00%   385985522.281   385946932.51       0.01%     2199    459   4973     7307k  2382.9s
-    # 501 =>     16940       0      6857 100.00%   409742237.0891  409701270.72       0.01%     2405    413   5303     3636k  1288.2s
+    # Var6 results
+    #   5 =>         1       0         1 100.00%   13900824.11     13900824.11        0.00%      107      9      5       563     1.6s
+    #  10 =>         1       0         1 100.00%   24954483.03     24954483.03        0.00%      217     11      4      1091     0.8s
+    #  20 =>         1       0         1 100.00%   44336605.295    44334018.51        0.01%       39      6      6      3157     1.5s
+    #  30 =>         1       0         1 100.00%   58540725        58540725           0.00%       39     22    305     11595     5.6s
+    #  50 =>         5       0         3 100.00%   84340234.96     84340234.96        0.00%       81     44    627     31409    11.8s
+    # 100 =>      1286       0       535 100.00%   137562435.03    137562435.03       0.00%      996    194   4867    277984    92.4s
+    # 150 =>      9449       0      3799 100.00%   183030044.3538  183012954.78       0.01%     1666    307   4873     1509k   572.9s
+    # 200 =>     34960       0     14080 100.00%   222778887.4344  222756620.54       0.01%     2092    449   4937     7365k  2749.1s
+    # 250 =>     36662       0     10922 100.00%   260995999.6362  260969949.12       0.01%     2650    496   5154     6371k  2559.2s
+    # 300 =>     30356       0     13247 100.00%   297920443.466   297890668.77       0.01%     2353    536   5328     5283k  1901.0s
+    # 350 =>     17919       0      8522 100.00%   332070195.4507  332036997.45       0.01%     1925    437   5151     3753k  1281.3s
+    # 400 =>     15475       0      5356 100.00%   361375133.5839  361339029          0.01%     2037    353   4947     2932k  1023.1s
+    # 450 =>     17917       0      6861 100.00%   385985476.6622  385946932.51       0.01%     2290    529   4941     3904k  1343.9s
+    # 501 =>     10849       0      3955 100.00%   409742166.3493  409701270.72       0.01%     2448    406   5017     2109k   767.0s
 
     # for budget in [5, 10, 20, 30, 50, 100, 150, 200, 250, 300, 350, 400, 450, 501]:
-    for budget in [5, 10, 20, 30, 50, 100, 150, 200, 250, 300, 350, 400, 450, 501]:
-        config["name"] = "EmpireSolver"
+    # for budget in [375, 395, 415, 435, 455, 475, 495, 515]:
+    for budget in [300, 350, 400, 450, 501]:
+        config["name"] = "EmpireSolverTest1"
         config["budget"] = budget
         config["top_n"] = 4
         config["nearest_n"] = 5
         config["waypoint_capacity"] = 25
-        config["solver"]["file_prefix"] = "NewBestYetAgain"
+        config["solver"]["file_prefix"] = "Var1"
         config["solver"]["file_suffix"] = ""
-        config["solver"]["mips_gap"] = "0.00005"
+        config["solver"]["mips_gap"] = "0.0001"
         config["solver"]["time_limit"] = "46800"
+        config["solver"]["random_seed"] = random.randint(0, 2147483647)
         empire_solver(config)
 
 
