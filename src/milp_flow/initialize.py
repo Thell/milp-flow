dabad699f933d14e2be4fd951a3fe9b52c4390bd
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-10-25 20:01:52 -0500

    data: cleanup initialize remnants

--- src/milp_flow/initialize.py
+++ src/milp_flow/initialize.py
@@ -1,6 +1,5 @@
 # initialize.py
 
-# import json
 import milp_flow.data_store as ds
 
 
@@ -10,16 +9,6 @@ workerman_data_filenames = [
     "plantzone_drops.json",
     "skills.json",
     "worker_static.json",
-    # Used for empire data generation
-    # "all_lodging_storage.json", # replacing
-    # "deck_links.json", # replacing
-    # "exploration.json", # replacing
-]
-
-local_data_filenames = [
-    # Used for empire data generation
-    # "townnames.json",
-    # "warehouse_to_townname.json",
 ]
 
 
@@ -36,6 +25,6 @@ def initialize_data() -> None:
     # Any error in initialization is fatal and is raised via urllib.
     print("Checking data files...")
     last_sha = ds.download_sha()
-    if not ds.initialized(last_sha, workerman_data_filenames + local_data_filenames):
+    if not ds.initialized(last_sha, workerman_data_filenames):
         initialize_workerman_data(last_sha)
     print("Initialized...")
