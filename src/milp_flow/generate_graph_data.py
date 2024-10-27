441402237e41a865d7676f5c157af7e048a89f6a
Thell 'Bo' Fowler <thell@tbfowler.net>
2024-10-27 15:17:17 -0500

    generate_graph_data: use exploration.json
    - make more use of new data setup.
    - prep for forced taken nodes and nearest_n overhaul.

--- src/milp_flow/generate_graph_data.py
+++ src/milp_flow/generate_graph_data.py
@@ -175,28 +175,27 @@ def add_arcs(nodes: Dict[str, Node], arcs: Dict[tuple, Arc], node_a: Node, node_
                 arc.destination.regions = [arc.source]
 
 
-def get_sparsified_link_graph(ref_data: Dict[str, Any]):
+def get_sparsified_link_graph(data: Dict[str, Any]):
     link_graph = nx.Graph()
-    for origin, origin_data in ref_data["exploration"].items():
-        for link_key in origin_data["link_list"]:
-            destination = ref_data["exploration"].get(link_key, None)
-            if not destination or destination["is_plantzone"]:
+    for origin_key, origin_data in data["exploration"].items():
+        for destination_key in origin_data["link_list"]:
+            if destination_key not in data["exploration"]:
                 continue
-            link_graph.add_edge(origin, link_key)
-    # for link in ref_data["waypoint_links"]:
-    #     link_graph.add_edge(link[0], link[1])
-    for node, data in link_graph.nodes(data=True):
-        data["weight"] = ref_data["exploration"][node]["need_exploration_point"]
-        data["type"] = get_link_node_type(node, ref_data)
+            destination_data = data["exploration"][destination_key]
+            if not destination_data["is_plantzone"]:
+                link_graph.add_edge(origin_key, destination_key)
+
+    for node, node_data in link_graph.nodes(data=True):
+        node_data["type"] = get_link_node_type(node, data)
 
     # This removes the non-plant non-forced leaf nodes without repeated pruning.
     # Testing showed that doing any other reductions reduces performance.
     removal_nodes = []
-    for node, data in link_graph.nodes(data=True):
+    for node, node_data in link_graph.nodes(data=True):
         if (
             nx.degree(link_graph, node) == 1
-            and data["type"] is not NodeType.plant
-            and node not in ref_data["force_active_node_ids"]
+            and node_data["type"] is not NodeType.plant
+            and node not in data["force_active_node_ids"]
         ):
             removal_nodes.append(node)
     if removal_nodes:
@@ -206,42 +205,32 @@ def get_sparsified_link_graph(ref_data: Dict[str, Any]):
     return link_graph
 
 
-def get_link_node_type(node_id: int, ref_data: Dict[str, Any]):
-    """Return the NodeType of the given node_id node.
-
-    - NodeType.INVALID indicates a node that is unused and not added to the graph.
-    """
-    if ref_data["exploration"][node_id]["is_town"]:
+def get_link_node_type(node_id: int, data: Dict[str, Any]):
+    """Return the NodeType of the given node_id node."""
+    if data["exploration"][node_id]["is_town"]:
         return NodeType.town
-    if ref_data["exploration"][node_id]["is_workerman_plantzone"]:
+    if data["exploration"][node_id]["is_workerman_plantzone"]:
         return NodeType.plant
     return NodeType.waypoint
 
 
-def get_link_nodes(nodes, link, ref_data, link_graph):
-    if not link_graph.has_node(link[0]) or not link_graph.has_node(link[1]):
-        return (None, None)
-
-    node_a_id, node_b_id = str(link[1]), str(link[0])
-    node_a_type = get_link_node_type(int(node_a_id), ref_data)
-    node_b_type = get_link_node_type(int(node_b_id), ref_data)
+def get_link_nodes(nodes, origin, destination, data):
+    node_a_type = get_link_node_type(origin, data)
+    node_b_type = get_link_node_type(destination, data)
+    node_a_id, node_b_id = str(origin), str(destination)
 
     # Ensure arc node order.
     if node_a_type > node_b_type:
         node_a_id, node_b_id = node_b_id, node_a_id
         node_a_type, node_b_type = node_b_type, node_a_type
 
-    if NodeType.INVALID in [node_a_type, node_b_type]:
-        # Not used in the graph because they are beyond the scope of the optimization.
-        return (None, None)
-
     return (
-        get_node(nodes, node_a_id, node_a_type, ref_data),
-        get_node(nodes, node_b_id, node_b_type, ref_data),
+        get_node(nodes, node_a_id, node_a_type, data),
+        get_node(nodes, node_b_id, node_b_type, data),
     )
 
 
-def get_node(nodes, node_id: str, node_type: NodeType, ref_data: Dict[str, Any], **kwargs) -> Node:
+def get_node(nodes, node_id: str, node_type: NodeType, data: Dict[str, Any], **kwargs) -> Node:
     """
     Generate, add and return node based on NodeType.
 
@@ -255,18 +244,18 @@ def get_node(nodes, node_id: str, node_type: NodeType, ref_data: Dict[str, Any],
 
     match node_type:
         case NodeType.𝓢:
-            ub = ref_data["max_ub"]
+            ub = data["max_ub"]
             cost = 0
         case NodeType.plant:
             ub = 1
-            cost = ref_data["exploration"][int(node_id)]["need_exploration_point"]
+            cost = data["exploration"][int(node_id)]["need_exploration_point"]
         case NodeType.waypoint | NodeType.town:
-            ub = ref_data["config"]["waypoint_ub"]
-            cost = ref_data["exploration"][int(node_id)]["need_exploration_point"]
+            ub = data["config"]["waypoint_ub"]
+            cost = data["exploration"][int(node_id)]["need_exploration_point"]
         case NodeType.region:
-            lodging_data = ref_data["lodging_data"][int(node_id)]
+            lodging_data = data["lodging_data"][int(node_id)]
             ub = lodging_data["max_ub"] + lodging_data["lodging_bonus"]
-            ub = min(ub, ref_data["config"]["waypoint_ub"])
+            ub = min(ub, data["config"]["waypoint_ub"])
             cost = 0
         case NodeType.lodging:
             ub = kwargs.get("ub")
@@ -278,7 +267,7 @@ def get_node(nodes, node_id: str, node_type: NodeType, ref_data: Dict[str, Any],
             ), "Lodging nodes require 'ub', 'lb' 'cost' and 'root' kwargs."
             regions = [root]
         case NodeType.𝓣:
-            ub = ref_data["max_ub"]
+            ub = data["max_ub"]
             cost = 0
         case NodeType.INVALID:
             assert node_type is not NodeType.INVALID, "INVALID node type."
@@ -286,7 +275,7 @@ def get_node(nodes, node_id: str, node_type: NodeType, ref_data: Dict[str, Any],
 
     node = Node(str(node_id), node_type, ub, lb, cost, regions)
     if node.key not in nodes:
-        if node_id in ref_data["force_active_node_ids"]:
+        if node_id in data["force_active_node_ids"]:
             node.isForceActive = True
         if node.type is NodeType.region:
             node.regions = [node]
@@ -295,74 +284,78 @@ def get_node(nodes, node_id: str, node_type: NodeType, ref_data: Dict[str, Any],
     return nodes[node.key]
 
 
-def process_links(nodes: Dict[str, Node], arcs: Dict[tuple, Arc], ref_data: Dict[str, Any]):
+def process_links(nodes: Dict[str, Node], arcs: Dict[tuple, Arc], data: Dict[str, Any]):
     """Process all waypoint links and add the nodes and arcs to the graph.
 
     Calls handlers for plant and town nodes to add plant value nodes and
     region/lodging nodes with their respective source and sink arcs.
     """
-    link_graph = get_sparsified_link_graph(ref_data)
+    link_graph = get_sparsified_link_graph(data)
 
-    for source_key, source_data in ref_data["exploration"].items():
-        for destination_key in source_data["link_list"]:
-            if destination_key not in ref_data["exploration"]:
-                continue
-
-            source, destination = get_link_nodes(
-                nodes, (source_key, destination_key), ref_data, link_graph
-            )
-            if source is None or destination is None:
-                continue
-
-            add_arcs(nodes, arcs, source, destination)
-
-            if source.isPlant:
-                process_plant(nodes, arcs, source, ref_data)
-            if destination.isTown:
-                process_town(nodes, arcs, destination, ref_data)
-
-
-def process_plant(
-    nodes: Dict[str, Node], arcs: Dict[tuple, Arc], plant: Node, ref_data: Dict[str, Any]
-):
-    """Add plant region values and arcs between the source and plant nodes."""
-    for i, (region_id, value_data) in enumerate(ref_data["plant_values"][plant.id].items(), 1):
-        if i > ref_data["config"]["top_n"]:
-            break
-        if value_data["value"] == 0:
+    for origin_key, origin_data in data["exploration"].items():
+        if not link_graph.has_node(origin_key):
             continue
-        value_data["value"]
+
+        for destination_key in origin_data["link_list"]:
+            if not link_graph.has_node(destination_key):
+                continue
+
+            # `get_link_nodes()` orders the nodes by type.
+            start_node, end_node = get_link_nodes(nodes, origin_key, destination_key, data)
+            add_arcs(nodes, arcs, start_node, end_node)
+
+            if start_node.isPlant:
+                process_plant(nodes, arcs, start_node, data)
+            if end_node.isTown:
+                process_town(nodes, arcs, end_node, data)
+
+
+def process_plant(nodes: Dict[str, Node], arcs: Dict[tuple, Arc], plant: Node, data: Dict[str, Any]):
+    """Add plant region values and arcs between the source and plant nodes."""
+    for i, (region_id, value_data) in enumerate(data["plant_values"][plant.id].items(), 1):
+        if i > data["config"]["top_n"]:
+            break
         plant.region_prizes[region_id] = value_data
 
     add_arcs(nodes, arcs, nodes["𝓢"], plant)
 
 
-def process_town(
-    nodes: Dict[str, Node], arcs: Dict[tuple, Arc], town: Node, ref_data: Dict[str, Any]
-):
+def process_town(nodes: Dict[str, Node], arcs: Dict[tuple, Arc], town: Node, data: Dict[str, Any]):
     """Add town region and lodging nodes and arcs between the town and sink nodes."""
-    exploration_node = ref_data["exploration"][int(town.id)]
+    exploration_node = data["exploration"][int(town.id)]
+
+    # TODO: change this for handling force taken nodes since they can connect to any base town.
     if not exploration_node["is_worker_npc_town"]:
         return
 
     region_key = exploration_node["region_key"]
-    lodging_data = ref_data["lodging_data"].get(region_key, None)
-    if lodging_data is None:
-        print("No lodging data found for:", exploration_node)
+    lodging_data = data["lodging_data"].get(region_key, None)
+    assert lodging_data, f"Error: Lodging data missing for region {region_key}!"
     lodging_bonus = lodging_data["lodging_bonus"]
 
+    # Region lodging data is ordered in ascending order by lodgings and cost.
+    # For each lodging count <= min(max_ub, lodging_bonus) find the lowest cost.
+
+    # lodgings is the list of previous 'best' (lodging, cost) pairs.
     lodgings = [(1 + lodging_bonus, 0)]
+
     for ub, lodging_data in lodging_data.items():
         if ub in ["max_ub", "lodging_bonus"]:
             continue
+
         current = (1 + lodging_bonus + int(ub), lodging_data[0].get("cost"))
+
+        # remove previous 'best' (lodging, cost) pairs when dominated and replace with new 'best'
         while lodgings and current[1] <= lodgings[-1][1] and current[0] >= lodgings[-1][0]:
             lodgings.pop(-1)
         lodgings.append(current)
-        if current[0] + 1 >= ref_data["config"]["waypoint_ub"]:
+
+        if current[0] + 1 >= data["config"]["waypoint_ub"]:
             break
 
-    region_node = get_node(nodes, region_key, NodeType.region, ref_data, ub=lodgings[-1][0])
+    # Each (lodging, cost) pair us a unique node in the graph, lodging is the arc flow constraint.
+
+    region_node = get_node(nodes, region_key, NodeType.region, data, ub=lodgings[-1][0])
     add_arcs(nodes, arcs, town, region_node)
 
     lb = 0
@@ -371,7 +364,7 @@ def process_town(
             nodes,
             f"{region_node.id}_for_{ub}",
             NodeType.lodging,
-            ref_data,
+            data,
             ub=ub,
             lb=lb,
             cost=cost,
@@ -382,22 +375,20 @@ def process_town(
         lb = ub + 1
 
 
-def nearest_n_towns(ref_data: Dict[str, Any], G: GraphData, nearest_n: int):
+def nearest_n_towns(data: Dict[str, Any], G: GraphData, nearest_n: int):
     waypoint_graph = nx.DiGraph()
     for arc in G["E"].values():
-        weight = 999999 if "1727" in arc.name() else arc.destination.cost
-        waypoint_graph.add_edge(arc.source.id, arc.destination.id, weight=weight)
+        waypoint_graph.add_edge(arc.source.id, arc.destination.id, weight=arc.destination.cost)
     all_pairs = dict(nx.all_pairs_bellman_ford_path_length(waypoint_graph, weight="weight"))
 
     nearest_towns_dist = {}
     nearest_towns = {}
-    region_to_town = {v["region_key"]: k for k, v in ref_data["exploration"].items() if v["is_town"]}
 
     for node_id, node in G["V"].items():
         if node.isWaypoint or node.isTown:
             distances = []
             for region in G["R"].values():
-                town_id = region_to_town[int(region.id)]
+                town_id = data["affiliated_town_region"][int(region.id)]
                 distances.append((region, all_pairs[node.id][str(town_id)]))
             nearest_towns_dist[node_id] = sorted(distances, key=lambda x: x[1])[:nearest_n]
             nearest_towns[node_id] = [w for w, _ in nearest_towns_dist[node_id]]
@@ -405,9 +396,9 @@ def nearest_n_towns(ref_data: Dict[str, Any], G: GraphData, nearest_n: int):
     return nearest_towns
 
 
-def finalize_regions(ref_data: Dict[str, Any], G: GraphData, nearest_n: int):
+def finalize_regions(data: Dict[str, Any], G: GraphData, nearest_n: int):
     # All region nodes have now been generated, finalize regions entries
-    nearest_towns = nearest_n_towns(ref_data, G, nearest_n)
+    nearest_towns = nearest_n_towns(data, G, nearest_n)
     for v in G["V"].values():
         if v.type in [NodeType.𝓢, NodeType.𝓣]:
             v.regions = [w for w in G["R"].values()]
@@ -417,15 +408,15 @@ def finalize_regions(ref_data: Dict[str, Any], G: GraphData, nearest_n: int):
             v.regions = [w for w in G["R"].values() if w.id in v.region_prizes.keys()]
 
 
-def generate_graph_data(ref_data):
+def generate_graph_data(data):
     """Generate and return a GraphData Dict composing the LP empire data."""
     print("Generating graph data...")
     nodes: Dict[str, Node] = {}
     arcs: Dict[tuple[str, str], Arc] = {}
 
-    get_node(nodes, "𝓢", NodeType.𝓢, ref_data)
-    get_node(nodes, "𝓣", NodeType.𝓣, ref_data)
-    process_links(nodes, arcs, ref_data)
+    get_node(nodes, "𝓢", NodeType.𝓢, data)
+    get_node(nodes, "𝓣", NodeType.𝓣, data)
+    process_links(nodes, arcs, data)
 
     G: GraphData = {
         "V": dict(sorted(nodes.items(), key=lambda item: item[1].type)),
@@ -435,6 +426,6 @@ def generate_graph_data(ref_data):
         "L": {k: v for k, v in nodes.items() if v.isLodging},
         "F": {k: v for k, v in nodes.items() if v.isForceActive},
     }
-    finalize_regions(ref_data, G, ref_data["config"]["nearest_n"])
+    finalize_regions(data, G, data["config"]["nearest_n"])
 
     return G
